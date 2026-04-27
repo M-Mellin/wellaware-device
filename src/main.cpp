8a61178d62b7f66c28573390bf6d8f49de03c205
Mattias Mellin <mm225vh@student.lnu.se>
2026-04-27 13:39:52 +0200

    Add unique id for Arduino device

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,8 @@
 #include "wifiSignal.h"
 #include "syncTime.h"
 #include "localData.h"
+#include "deviceId.h"
+#include <esp_system.h>
 
 const int FLOAT_PIN = A1;
 const int ECHO_PIN = 4;
@@ -15,7 +17,7 @@ const int TRIG_PIN = 5;
 const char* ssid = WIFI_SSID;
 const char* password = WIFI_PASSWORD;
 
-const String DEVICE_ID = "Device-X1";
+const String DEVICE_ID = getDeviceId();
 
 const int MAX_MEASUREMENTS = 500;
 
